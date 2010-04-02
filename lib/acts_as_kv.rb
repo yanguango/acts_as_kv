b68179dd57e4f5a4cb3c0cf9943310c501035f3d
rociiu <rociiu.yu@gmail.com>
2010-04-02 22:43:42 +0800

    implement basic acts_as_kv

--- lib/acts_as_kv.rb
+++ lib/acts_as_kv.rb
@@ -2,12 +2,38 @@ module ActsAsKv
   
   def self.included(base)
     base.extend(ClassMethods)
+    base.send(:serialize, :value)
   end
+  
+  class ValueNotFound < StandardError; end
   
   module ClassMethods
   
-    def get
-      
+    def get(key)
+      object = self.find_by_key(key.to_s)
+      if object
+        object.value
+      else
+        raise ValueNotFound.new("Value for key: #{key} not found")
+      end
+    end
+    
+    def set(key, value)
+      object = self.find_by_key(key.to_s)
+      if object
+        object.value = value
+      else
+        object = self.new( { :key => key.to_s, :value => value } )
+      end
+      object.save
+      object.value
+    end
+    
+    def drop(key)
+      object = self.find_by_key(key.to_s)
+      if object
+        object.destroy
+      end
     end
     
   end
--- spec/acts_as_kv_spec.rb
+++ spec/acts_as_kv_spec.rb
@@ -6,21 +6,18 @@ describe "ActsAsKv" do
   before(:each) do
     KvDb.send(:include, ActsAsKv)
   end
-  
+    
   describe ".get, .set" do
     
     it "should have get method" do
-      pending
       KvDb.respond_to?(:get).should == true
     end
 
     it "should have set method" do
-      pending
       KvDb.respond_to?(:set).should == true
     end
 
     it "should get correct value for a specify key" do
-      pending
       value = { :a => 1, :b => 2}
       KvDb.set(:key_a, value)
       KvDb.get(:key_a).should == value
@@ -31,16 +28,14 @@ describe "ActsAsKv" do
   describe ".drop" do
     
     it "should have drop method" do
-      pending
       KvDb.respond_to?(:drop).should == true
     end
     
     it "should drop the kv" do
-      pending
       KvDb.set(:key_c, { :a => 1})
       KvDb.get(:key_c).should == { :a => 1}
       KvDb.drop(:key_c)
-      KvDb.get(:key_c).should raise_error
+      lambda { KvDb.get(:key_c) }.should raise_error
     end
     
   end
